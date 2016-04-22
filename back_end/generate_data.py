25208d690b1bd257bb69fa70a2b033aa26608dcd
Alexander Elmekeev <donamok@gmail.com>
2016-04-22 15:45:37 +0300

    - completed work on generate data back end script; - initial version of front end code; - small fixes in input data.

--- back_end/generate_data.py
+++ back_end/generate_data.py
@@ -2,6 +2,7 @@
 
 import csv
 import json
+from datetime import datetime
 
 EPISODE_NUMBER = 0
 EPISODE_NAME = 1
@@ -15,13 +16,22 @@ LINK_ENG_XL = 8
 LINK_RUS_XL = 9
 
 ALAN = 'Alan Davies'
+STEPHEN = 'Stephen Fry'
+THE_AUDIENCE = 'The Audience'
 NO_GUESTS = ['', 'N/A', 'Compilation episode']
 
 links = set()
 seasons = []
-people = [ALAN]
+people = [{'name': STEPHEN, 'shows': [], 'winner': []}, {'name': ALAN, 'shows': [], 'winner': []}]
 episodes = []
-guests = []
+
+
+def format_date(date_string):
+  if ' ' in date_string:
+    date = datetime.strptime(date_string, '%d %B %Y')
+    return datetime.strftime(date, '%d/%m/%Y')
+  else:
+    return date_string
 
 
 def add_episode(row, current_season):
@@ -31,31 +41,50 @@ def add_episode(row, current_season):
   episode['name'] = row[EPISODE_NAME][1:-1]
   
   number = row[EPISODE_NUMBER].split()
-  episode['e#'] = number[0]
+  episode['episode'] = number[0]
   if len(number) != 1:
-    episode['#'] = number[1][1:-1]
-    
-  episode['eng'] = row[LINK_ENG]
-  episode['rus'] = row[LINK_RUS]
-  episode['eng_xl'] = row[LINK_ENG_XL]
-  episode['rus_xl'] = row[LINK_RUS_XL]
+    episode['episode_total'] = number[1][1:-1]
+  
+  if row[LINK_ENG] != '': episode['eng'] = row[LINK_ENG][44:]
+  if row[LINK_RUS] != '': episode['rus'] = row[LINK_RUS][44:]
+  if row[LINK_ENG_XL] != '': episode['eng_xl'] = row[LINK_ENG_XL][44:]
+  if row[LINK_RUS_XL] != '': episode['rus_xl'] = row[LINK_RUS_XL][44:]
 
   air_dates = row[AIR_DATE].split('\n')
-  episode['air'] = air_dates[0]
-  if len(air_dates) != 1:
-    episode['air_xl'] = air_dates[1][:-13]
+  for date in air_dates:
+    if 'XL edit' in date:
+      episode['air_xl'] = format_date(date[:-13])
+    else:
+      episode['air'] = format_date(date)
   
   episodes.append(episode)
+
   
+def log_guest(row, guest_name, episode_id):
+  new_guest = False
+  guest = next((x for x in people if x['name'] == guest_name), None)
+  if guest is None:
+    new_guest = True
+    guest = {'name': guest_name, 'shows': [], 'winner': []}
+  guest['shows'].append(episode_id)
+  if guest_name in row[WINNER]:
+    guest['winner'].append(episode_id)
+  if new_guest:
+    people.append(guest)
   
-def log_guest(row, guest_name):
-  guest = {}
-  
-  guest['show_id'] = len(episodes) - 1
-  guest['people_id'] = people.index(guest_name)
-  guest['is_winner'] = guest_name in row[WINNER]
-  
-  guests.append(guest)
+
+def verify_winners():
+  shows_with_winners = set()
+  all_shows_ids = set(range(0, len(episodes) -1 ))
+        
+  for participant in people:
+    for win in participant['winner']:
+      shows_with_winners.add(win)
+
+  print('The following shows will not be marked as "winner" for any guest:', '\n')
+  for show in all_shows_ids.difference(shows_with_winners):
+    episode = episodes[show]
+    print(seasons[episode['season']], episode['episode'], '-', episode['name'])
   
 
 with open('../data/data.csv', encoding='utf-8', mode='r') as input, open('../data/data.js', encoding='utf-8', mode='w') as output:
@@ -76,15 +105,28 @@ with open('../data/data.csv', encoding='utf-8', mode='r') as input, open('../dat
       current_season += 1
     else:
       add_episode(row, current_season)
+      episode_id = len(episodes) - 1
       
       panel = row[GUESTS].split('\n')
-      for guest in panel:
-        if guest not in NO_GUESTS:
-          if guest not in people:
-            people.append(guest)
-          log_guest(row, guest)
-          
-      if 'No Alan' not in row[NOTE] and row[GUESTS] not in NO_GUESTS:
-        log_guest(row, ALAN)
+      for guest_name in panel:
+        if guest_name not in NO_GUESTS:
+          log_guest(row, guest_name, episode_id)
+            
+      log_guest(row, STEPHEN, episode_id)
+      log_guest(row, THE_AUDIENCE, episode_id)
+      if 'No Alan' not in row[NOTE]:
+        log_guest(row, ALAN, episode_id)
 
-  print(episodes)
+  verify_winners()
+
+  output.write('var seasons=')
+  output.write(json.dumps(seasons))
+  output.write(';')
+  
+  output.write('var shows=')
+  output.write(json.dumps(episodes))
+  output.write(';')
+  
+  output.write('var guests=')
+  output.write(json.dumps(sorted(people, key=lambda x: x['name'])))
+  output.write(';')
